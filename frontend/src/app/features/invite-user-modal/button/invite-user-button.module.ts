1744d93336b7b586dc0380f22b71df8f5413e254
Tiffany Bao <tiffany.yzh@gmail.com>
2022-04-12 04:30:57 -0700

    clean up

--- frontend/src/app/features/invite-user-modal/button/invite-user-button.module.ts
+++ frontend/src/app/features/invite-user-modal/button/invite-user-button.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InviteUserButtonComponent } from 'core-app/features/invite-user-modal/button/invite-user-button.component';
-import { CCButtonComponent } from 'core-app/features/invite-user-modal/button/cc-button.component';
 import { IconModule } from 'core-app/shared/components/icon/icon.module';
 
 @NgModule({
   declarations: [
     InviteUserButtonComponent,
-    CCButtonComponent,
   ],
   imports: [
     CommonModule,
@@ -15,7 +13,6 @@ import { IconModule } from 'core-app/shared/components/icon/icon.module';
   ],
   exports: [
     InviteUserButtonComponent,
-    CCButtonComponent,
   ],
 })
 export class InviteUserButtonModule { }
